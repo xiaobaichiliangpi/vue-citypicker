0400d410050407be76a0323dab682f5110267f78
weiwei <weiwei@suiyi.com.cn>
2017-08-30 09:24:41 +0800

    更新兑换axios

--- src/axios/exchange.js
+++ src/axios/exchange.js
@@ -1,9 +1,63 @@
 import axios from './simpleAxios'
+import store from '../store'
 
 export function showProductByCard (data = {}) {
   return axios({
     method: 'POST',
     url: `/pickupcard/api/getproductbycardnumandpassword`,
     data: data
+  })
+}
+
+export function listCity () {
+  return axios({
+    method: 'GET',
+    url: `/pickupcard/api/cityinfo`
+  })
+}
+
+export function listUserAddress () {
+  return axios({
+    method: 'GET',
+    url: `/pickupcard/api/${(store.getters.city && store.getters.city.CityFlag) || 'sz'}/address/${store.getters.token.CustomerGuid}`
+  })
+}
+
+export function createOrUpdateAddress (data = {}) {
+  return axios({
+    method: 'POST',
+    url: `/pickupcard/api/${(store.getters.city && store.getters.city.CityFlag) || 'sz'}/address/${store.getters.token.CustomerGuid}`,
+    data: data
+  })
+}
+
+export function deleteAddress (data = {}) {
+  return axios({
+    method: 'DELETE',
+    url: `/pickupcard/api/${(store.getters.city && store.getters.city.CityFlag) || 'sz'}/address/${store.getters.token.CustomerGuid}`,
+    data: data
+  })
+}
+
+export function listArea () {
+  return axios({
+    method: 'GET',
+    url: `/pickupcard/api/${(store.getters.city && store.getters.city.CityFlag) || 'sz'}/areas/`
+  })
+}
+
+export function listStation (params = {}) {
+  return axios({
+    method: 'GET',
+    url: `/pickupcard/api/${(store.getters.city && store.getters.city.CityFlag) || 'sz'}/station`,
+    params: params
+  })
+}
+
+export function listStationTime (params = {}) {
+  return axios({
+    method: 'GET',
+    url: `/pickupcard/api/${(store.getters.city && store.getters.city.CityFlag) || 'sz'}/areas/${store.getters.token.CustomerGuid}`,
+    params: params
   })
 }
--- src/axios/sign.js
+++ src/axios/sign.js
@@ -1,4 +1,5 @@
 import axios from './axios'
+import simpleAxios from './simpleAxios'
 import { encryptDES } from '../utils/encryptDES'
 // import env from '../env'
 import store from '../store'
@@ -11,6 +12,18 @@ export function signIn ({ username, password }) {
       Phone: username,
       PassWord: encryptDES(password, '8D54E5D0'),
       sourcetype: store.getters.SourceType
+    }
+  })
+}
+
+export function exchangeSignIn ({ username, password }) {
+  return simpleAxios({
+    method: 'POST',
+    url: `/pickupcard/api/${(store.getters.exCity && store.getters.exCity.CityFlag) || 'sz'}/login`,
+    data: {
+      phone: username,
+      passWord: encryptDES(password, '8D54E5D0'),
+      sourceType: store.getters.SourceType
     }
   })
 }
